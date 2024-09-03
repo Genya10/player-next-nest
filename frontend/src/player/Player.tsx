19be485af9b95cbd71bc4d435cecaa7434ef2f20
Jenya <113167825+Genya10@users.noreply.github.com>
2024-09-03 19:35:09 +0300

    create enum, functions togglePlayPause and skipTime to Player.tsx

--- frontend/src/player/Player.tsx
+++ frontend/src/player/Player.tsx
@@ -1,4 +1,32 @@
+import { useRef, useState } from "react"
+import { EnumPlayerQuality } from "./playertypes"
+
+const SKIP_SECONDS_TIME = 15
+
 export function Player(){
+    const playerRef = useRef<HTMLVideoElement>(null)
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [quality, setQuality] = useState(EnumPlayerQuality['1080p'])
+
+    const togglePlayPause = () => {
+        if(isPlaying){
+            playerRef.current?.pause()
+        } else {
+            playerRef.current?.play()
+        }
+        setIsPlaying(!isPlaying)
+    }
+
+    const skipTime = (type?: 'forward' | 'backward') =>{
+      if(!playerRef.current?.currentTime) return
+
+      if(type === 'forward'){
+        playerRef.current.currentTime += SKIP_SECONDS_TIME
+      } else {
+        playerRef.current.currentTime -= SKIP_SECONDS_TIME
+      }
+    }
+
     return(
         <div>
             Player
---
+++ frontend/src/player/playertypes.ts
@@ -0,0 +1,7 @@
+export enum EnumPlayerQuality {
+    'original' = 'original',
+    '1080p' = '1080p',
+    '720p' = '720p',
+    '480p' = '480p',
+    '360p' = '360p',
+}
